4f7d3f2464e67039ba83e9efdd236c386d4ae1f0
Towhidmuhiuddin <mdtawhidmuhiuddin2020@gmail.com>
2023-02-24 15:44:22 +0600

    8th commit

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo " BUILD START"
-python3.11 -m pip install -r requirements.txt
-python3.11 manage.py collectstatic --noinput --clear
+python3.10 -m pip install -r requirements.txt
+python3.10 manage.py collectstatic --noinput --clear
 echo " BUILD END" 
--- movie/movie/settings.py
+++ movie/movie/settings.py
@@ -134,8 +134,6 @@ MEDIA_ROOT = BASE_DIR/'static'
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 #static work
-import os
-STATICFILES_DIRS =os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 
