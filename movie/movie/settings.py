89b8fd5bba375fbcb6715e64a459f15cefbd3235
Towhidmuhiuddin <mdtawhidmuhiuddin2020@gmail.com>
2023-02-24 12:45:07 +0600

    done vercel

--- movie/movie/settings.py
+++ movie/movie/settings.py
@@ -137,7 +137,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 #static work
 import os
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATICFILES_DIRS =os.path.join(BASE_DIR,'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
